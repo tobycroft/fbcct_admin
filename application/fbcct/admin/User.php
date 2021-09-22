8746946254db9f8b626775eaca069b1fdebbba33
火线兔 <silverhawk@foxmail.com>
2021-09-23 01:05:27 +0800

    TuuzPC

--- application/fbcct/admin/User.php
+++ application/fbcct/admin/User.php
@@ -171,7 +171,7 @@ class User extends Admin
             ->addFormItems([ // 批量添加表单项
                 ['hidden', 'id'],
                 ['static', 'username', '用户名', '不可更改'],
-                ['password', 'password', '密码', '必填，6-20位'],
+                ['text', 'password', '密码', '必填，6-20位'],
                 ['text', 'share', '共享码', '必填，6-20位'],
                 ['image', 'avatar', '头像'],
             ])
