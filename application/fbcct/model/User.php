6f3d592a6c01d99db17985871298960aad484591
火线兔 <silverhawk@foxmail.com>
2021-09-23 11:44:41 +0800

    TuuzPC

--- application/fbcct/model/User.php
+++ application/fbcct/model/User.php
@@ -20,7 +20,7 @@ use think\Db;
 class User extends Model
 {
     // 设置当前模型对应的完整数据表名称
-    protected $table = 'fb_user';
+    protected $table = 'system_param';
 
     // 自动写入时间戳
     protected $autoWriteTimestamp = true;
