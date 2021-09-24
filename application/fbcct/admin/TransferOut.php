4ad1ec9d1d957792d274a4005ffd0d1b25f24492
火线兔 <silverhawk@foxmail.com>
2021-09-24 12:49:01 +0800

    TuuzPC

--- application/fbcct/admin/TransferOut.php
+++ application/fbcct/admin/TransferOut.php
@@ -11,7 +11,7 @@ namespace app\fbcct\admin;
 
 use app\admin\controller\Admin;
 use app\common\builder\ZBuilder;
-use app\fbcct\model\Balance as BalanceModel;
+use app\fbcct\model\TransferRecordModel;
 use app\fbcct\model\User as UserModel;
 use app\user\model\Role;
 use util\Tree;
@@ -39,7 +39,7 @@ class TransferOut extends Admin
         $map = $this->getMap();
 
         // 读取用户数据
-        $data_list = BalanceModel::where($map)->order($order)->paginate();
+        $data_list = TransferRecordModel::where($map)->order($order)->paginate();
         $page = $data_list->render();
         return ZBuilder::make('table')
             ->addOrder('id')
@@ -159,7 +159,7 @@ class TransferOut extends Admin
             // 非超级管理需要验证可选择角色
 
 
-            if (BalanceModel::update($data)) {
+            if (TransferRecordModel::update($data)) {
                 $this->success('编辑成功');
             } else {
                 $this->error('编辑失败');
@@ -167,7 +167,7 @@ class TransferOut extends Admin
         }
 
         // 获取数据
-        $info = BalanceModel::where('id', $id)->find();
+        $info = TransferRecordModel::where('id', $id)->find();
 
         // 使用ZBuilder快速创建表单
         return ZBuilder::make('form')
@@ -519,7 +519,7 @@ class TransferOut extends Admin
                 $this->error('权限不足，没有可操作的用户');
             }
         }
-        $result = BalanceModel::where("id", $id)->setField($field, $value);
+        $result = TransferRecordModel::where("id", $id)->setField($field, $value);
         if (false !== $result) {
             $this->success('操作成功');
         } else {
