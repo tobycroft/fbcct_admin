1676ed45a666c83e880d4aefbbafac2e480e70c7
火线兔 <silverhawk@foxmail.com>
2021-09-24 12:51:52 +0800

    TuuzPC

--- application/fbcct/admin/TransferOut.php
+++ application/fbcct/admin/TransferOut.php
@@ -50,7 +50,7 @@ class TransferOut extends Admin
             ->addColumn('fee', '目标地址')
             ->addColumn('amount', '目标地址')
             ->addColumn('approved', '是否通过', 'status', '', [-1 => '不通过:danger', '待通过', '通过', '未知'])
-            ->addColumn('status', '当前状态', 'status', '', [-1 => '拒绝:danger', '待处理', '已完成', '未知'])
+            ->addColumn('status', '当前状态', 'status', '', [-1 => '失败:danger', '待处理', '已完成', '未知'])
             ->addColumn('change_date', '目标地址')
             ->addColumn('date', '目标地址')
             ->setRowList($data_list) // 设置表格数据
