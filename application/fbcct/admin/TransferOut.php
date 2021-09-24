a0b10360e13eee5c1caa065595284d2792d41392
火线兔 <silverhawk@foxmail.com>
2021-09-24 13:21:24 +0800

    TuuzPC

--- application/fbcct/admin/TransferOut.php
+++ application/fbcct/admin/TransferOut.php
@@ -45,13 +45,13 @@ class TransferOut extends Admin
         $btn = [
             'title' => '已完成',
             'icon' => 'fa fa-fw fa-yes',
-            'href' => url('yes', ['uid' => '__id__'])
+            'href' => url('yes', ['id' => '__id__'])
         ];
 
         $btn1 = [
             'title' => '失败',
             'icon' => 'fa fa-fw fa-no',
-            'href' => url('no', ['uid' => '__id__'])
+            'href' => url('no', ['id' => '__id__'])
         ];
 
         return ZBuilder::make('table')
