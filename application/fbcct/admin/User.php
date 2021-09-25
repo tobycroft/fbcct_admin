815493153e2e63897ec7de39ab34ce4d2b7062d8
火线兔 <silverhawk@foxmail.com>
2021-09-26 00:51:38 +0800

    TuuzPC

--- application/fbcct/admin/User.php
+++ application/fbcct/admin/User.php
@@ -186,7 +186,7 @@ class User extends Admin
                 ['static', 'username', '用户名', '不可更改'],
                 ['text', 'password', '密码', '必填，6-20位'],
                 ['text', 'share', '共享码', '必填，6-20位'],
-                ['pic', 'head_img', '头像'],
+                ['image', 'head_img', '头像'],
             ])
             ->setFormData($info) // 设置表单数据
             ->fetch();
