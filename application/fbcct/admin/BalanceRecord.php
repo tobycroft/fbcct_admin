c690929dc5820fd91023318477f33c8b64ef80bb
火线兔 <silverhawk@foxmail.com>
2021-09-25 14:22:43 +0800

    TuuzPC

--- application/fbcct/admin/BalanceRecord.php
+++ application/fbcct/admin/BalanceRecord.php
@@ -35,7 +35,7 @@ class BalanceRecord extends Admin
     public function index()
     {
         // 获取排序
-        $order = $this->getOrder('id desc,sort');
+        $order = $this->getOrder('id desc');
         $map = $this->getMap();
 
 // 读取用户数据
