3c59a571b38ce8d2234e15e1a5a6f968ff7d758a
火线兔 <silverhawk@foxmail.com>
2021-09-25 23:31:51 +0800

    TuuzPC

--- application/admin/controller/Attachment.php
+++ application/admin/controller/Attachment.php
@@ -145,7 +145,7 @@ class Attachment extends Admin
         }
         $file = $this->request->file($file_input_name);
 
-        $post_data = array('file' => new \CURLFile($file->getRealPath(), $file->getType(), $file->getFilename()));
+        $post_data = array('file' => new \CURLFile($file->getRealPath()));
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, config('upload_url'));
