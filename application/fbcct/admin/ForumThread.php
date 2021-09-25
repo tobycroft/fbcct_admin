8dba80391bd9a446088df558cf8e8e79694f93e9
火线兔 <silverhawk@foxmail.com>
2021-09-25 16:15:38 +0800

    TuuzPC

--- application/fbcct/admin/ForumThread.php
+++ application/fbcct/admin/ForumThread.php
@@ -83,7 +83,7 @@ class ForumThread extends Admin
             }
         }
 
-        $data = ForumModel::where("")->select();
+        $data = ForumModel::select();
         $arr = [];
         foreach ($data as $item) {
             $arr[$data["name"]] = $data["id"];
