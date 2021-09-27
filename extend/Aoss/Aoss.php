64d4a1b441d3d93bd10a625af5ca322eeb512584
火线兔 <silverhawk@foxmail.com>
2021-09-28 00:32:33 +0800

    TuuzPC

--- extend/Aoss/Aoss.php
+++ extend/Aoss/Aoss.php
@@ -17,7 +17,7 @@ class Aoss
             'file' => new \CURLFile(realpath($real_path), $mime_type, $file_name)
         ];
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, self::$send_url);
+        curl_setopt($ch, CURLOPT_URL, $send_url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
