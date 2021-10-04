ab214528df144f951d8fb1fc394e5177ea399c5a
火线兔 <silverhawk@foxmail.com>
2021-10-04 11:23:16 +0800

    TuuzPC

--- application/fbcct/admin/BalanceRecord.php
+++ application/fbcct/admin/BalanceRecord.php
@@ -42,7 +42,10 @@ class BalanceRecord extends Admin
         $data_list = BalanceRecordModel::where($map)->order($order)->paginate();
         $page = $data_list->render();
         return ZBuilder::make('table')
-            ->setSearch(['uid' => 'UID', "order_id" => "订单号","daterange"=>"date"]) // 设置搜索参数
+            ->setSearch(['uid' => 'UID', "order_id" => "订单号"]) // 设置搜索参数
+            ->setSearchArea([
+                ['daterange', 'date', '日期'],
+            ])
             ->addOrder('id')
             ->addColumn('id', 'id')
             ->addColumn('uid', '用户id')
