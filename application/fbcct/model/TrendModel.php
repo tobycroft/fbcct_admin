c3ab9b8fe401f0258fa325a489a20bddc5ea2017
Tuuz <miracle-plus@outlook.com>
2021-10-21 11:28:27 +0800

    TuuzMac

--- application/fbcct/model/TrendModel.php
+++ application/fbcct/model/TrendModel.php
@@ -20,7 +20,7 @@ use think\Db;
 class TrendModel extends Model
 {
     // 设置当前模型对应的完整数据表名称
-    protected $table = 'fb_forum_thread';
+    protected $table = 'trend';
 
     // 自动写入时间戳
     protected $autoWriteTimestamp = true;
