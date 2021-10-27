e62426fb93b8db708fcf0fb06046ff715b87a124
Tuuz <miracle-plus@outlook.com>
2021-10-27 10:12:12 +0800

    TuuzMac

--- application/fbcct/model/ComputeRecordModel.php
+++ application/fbcct/model/ComputeRecordModel.php
@@ -19,7 +19,7 @@ use think\Db;
  */
 class ComputeRecordModel extends Model {
 	// 设置当前模型对应的完整数据表名称
-	protected $table = 'compute_order';
+	protected $table = 'compute_record';
 
 	// 自动写入时间戳
 	protected $autoWriteTimestamp = true;
